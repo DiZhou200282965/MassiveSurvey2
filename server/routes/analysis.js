842cd5b62a5cca4f9fe088f4febbfd1071bd0c23
DiZhou200282965 <chaodyz@gmail.com>
2015-12-12 03:08:29 -0500

    Dec12
    注意! survey一定要做过才能进analysis看详情
    如果survey没有人做过,那么tempSurveyAnsArry里面的属性可能为undifined 会导致网站崩溃

--- server/routes/analysis.js
+++ server/routes/analysis.js
@@ -7,101 +7,149 @@ var SurveyAnswer = require('../models/surveyanswer.js');
 var Survey = require('../models/survey.js');
 
 /* check if user is authenticatd */
-function requireAuth(req, res, next){
-  if(!req.isAuthenticated()){
-    return res.redirect('/login');
-  }
-  next();
+function requireAuth(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login');
+    }
+    next();
 };
 
 // Get survey list under current user
-router.get('/', requireAuth, function(req, res, next) {    
-    Survey.find({username: req.user.displayName}, function(err, surveys) {
-        if (err) 
-        {
+router.get('/', requireAuth, function (req, res, next) {
+    Survey.find({ username: req.user.username }, function (err, surveys) {
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else
-        {
+        else {
+            console.log(surveys.length);
             res.render('analysis/index', {
                 title: 'Analysis',
                 displayName: req.user ? req.user.displayName : '',
-                surveys: surveys              
+                surveys: surveys
             });
         }
     });
 });
 
 // GET the current survey analysis
-router.get('/:id', requireAuth, function(req, res, next) {
+router.get('/:id', requireAuth, function (req, res, next) {
     var id = req.params.id;
 
+    // get the current survey
+    //#############################Original code
+    //var tempSurvey = Survey.find({_id: id}, function(err,tempSurvey){
+    //  tempSurveyA = tempSurvey;
+    //  console.log(tempSurveyA.name);
+    //################################## modified block ###############
+    Survey.findById(id, function (err, survey) {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            var tempSurvey = survey;            
+            console.log("###### Name :"+tempSurvey.name); // for test###
+            SurveyAnswer.find({ surveyId: id }, function (err, surveyAnsArry) {
+                if (err)
+                {
+                    console.log(err);
+                    res.end(err);
+                }
+                else
+                {
+                    var tempSurveyAnsArry = surveyAnsArry;
 
-    // // get the current survey
-    // var tempSurvey = Survey.find({_id: id}, function(err,tempSurvey){
-    //   tempSurveyA = tempSurvey;
-    //   console.log(tempSurveyA.name);
+                    //### !!!!如果survey没有人做过，那么tempSurveyAnsArry里面的属性可能为undifined 会导致错误!!!!
+              
+                    console.log("###### tempSurveyAnsArry length :" + tempSurveyAnsArry.length);// for test###
+                    console.log("###### answer :" + tempSurveyAnsArry[0].twOptionAns[0].twoOptionAns);// for test###
+                    // start of calculation
+                    
+                    // for tow Option
+                    var opt1Count = 0;
+                    for (var i = 0; i < tempSurveyAnsArry.length; i++)
+                    {
+                        if (tempSurveyAnsArry[i].twOptionAns[0].twoOptionAns === tempSurvey.twoOption[0].option1)
+                            opt1Count++;               
+                    }
+                    console.log("###### Count :" + opt1Count);// for test###
+                    //end of calculation
+                    res.render('analysis/result', {
+                        title: "Survey Answer Details",
+                        displayName: req.user ? req.user.displayName : '',
+                        tempSurveyAnsArry: tempSurveyAnsArry,
+                        tempSurvey: tempSurvey,
+                        opt1Count: opt1Count,
+                    });
+                }
+            });// end of surveyAnswer find by ID
+        }
+    });// end of survey find by ID
+    
+});// end of get /:Id
+
+//  var SurveyAnswerA = SurveyAnswer.find({surveyId: id}, function(err, SurveyAnswer) {
+//  SurveyAnswerA = SurveyAnswer;
+// // console.log(SurveyAnswerA);
+//    // analyzing choice of two-option questions
+//  var option1Arr = [];
+//  var option2Arr = [];    
+//  for (var i = 0; i < SurveyAnswer.length; i++) 
+//  {
+// //   console.log(i);
+//    // for each question
+//    var option1 = 0;
+//  //  console.log(option1);
+//    var option2 = 0;
+//  //  console.log(tempSurvey.name);
+//    for (var j = 0; j < SurveyAnswerA[i].twOptionAns.length; j++) 
+//    {
+//   //   console.log(j);
+//      if (SurveyAnswerA[i].twOptionAns[j].twoOptionAns == tempSurveyA.twoOption[j].option1)
+//      {
+//        option1 = option1 + 1;
+//    //    console.log(option1);
+//      }
+//      if (SurveyAnswerA[i].twOptionAns[j].twoOptionAns == tempSurveyA.twoOption[j].option2)
+//      {
+//        option2++;
+//      }
+//    }      
+//    option1Arr[i] = option1;
+//  //  console.log(option1Arr);
+//    option1Arr.push(option2);
+//  }
+////  console.log(option1Arr);
+
+//    });
 
 
-    //   var SurveyAnswerA = SurveyAnswer.find({surveyId: id}, function(err, SurveyAnswer) {
-    //   SurveyAnswerA = SurveyAnswer;
-    //   console.log(SurveyAnswerA);
-    //     // analyzing choice of two-option questions
-    //   var option1Arr = [];
-    //   var option2Arr = [];    
-    //   for (var i = 0; i < SurveyAnswer.length; i++) 
-    //   {
-    //     console.log(i);
-    //     // for each question
-    //     var option1 = 0;
-    //     console.log(option1);
-    //     var option2 = 0;
-    //     console.log(tempSurvey.name);
-    //     for (var j = 0; j < SurveyAnswerA[i].twOptionAns.length; j++) 
-    //     {
-    //       console.log(j);
-    //       if (SurveyAnswerA[i].twOptionAns[j].twoOptionAns == tempSurveyA.twoOption[j].option1)
-    //       {
-    //         option1 = option1 + 1;
-    //         console.log(option1);
-    //       }
-    //       if (SurveyAnswerA[i].twOptionAns[j].twoOptionAns == tempSurveyA.twoOption[j].option2)
-    //       {
-    //         option2++;
-    //       }
-    //     }      
-    //     option1Arr[i] = option1;
-    //     console.log(option1Arr);
-    //     option1Arr.push(option2);
-    //   }
-    //   console.log(option1Arr);
-
-    // });
+//     }
+// });
+////   console.log(tempSurvey);
+//  // get the answers for this survey
 
 
-    // });
-    // // console.log(tempSurvey);
-    // // get the answers for this survey
-
-
-    SurveyAnswer.find({surveyId: id}, function(err, SurveyAnswer) {
-      if (err) 
-      {
-        console.log(err);
-        res.end(err);
-      }
-      else 
-      {
-         res.render('analysis/result', {
-            title: "Survey Answer Details",
-            displayName: req.user ? req.user.displayName : '',
-            surveyanswers: SurveyAnswer
-            // tempSurvey: tempSurvey,
-          });
-      }
-    });
-});
+// SurveyAnswer.find({surveyId: id}, function(err, SurveyAnswer) {
+//   if (err) 
+//   {
+//     console.log(err);
+//     res.end(err);
+//   }
+//   else 
+//   {
+//      res.render('analysis/result', {
+//         title: "Survey Answer Details",
+//         displayName: req.user ? req.user.displayName : '',
+//         surveyanswers: SurveyAnswer
+//          tempSurvey: tempSurvey,
+//       });
+//   }
+// });
+//});
 
 
 
